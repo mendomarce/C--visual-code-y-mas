5c1c2b0981334be4e7a6052800e318f300ee7e1f
Marcelo Mendoza <mendo.marce@gmail.com>
2024-07-10 20:40:40 -0300

    Primer commit

---
+++ EstudiandoCShare/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Se ejecuta con F5 o con el boton de play
+Live preview es una extension que te permite ver webs con F5 en visual code
+con ctrl +- hace todo mas grande o chico
+git es un software de control de version y github es una plataforma de git que almacena en la nube
+en visual code se crea un repositorio con el icono de control de versiones de la izquierda
+git: rename te permite renombrar una rama
+en git la U al lado de un archivo significa untracked y eso quiere decir que no esta siendo seguido por el repositorio y hay que agregarlo a el
+al hacer un stage sobre estos archivos ahora son seguidos con git y se señala con una A que fueron añadidos al git
+Console.WriteLine("Hello, World!");
