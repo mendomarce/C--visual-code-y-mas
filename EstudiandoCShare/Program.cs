d2ef5b04936b25557764953d31fcdcc7e3901007
Marcelo Mendoza <mendo.marce@gmail.com>
2024-07-10 20:58:52 -0300

    primer cambio en rama

--- EstudiandoCShare/Program.cs
+++ EstudiandoCShare/Program.cs
@@ -1,9 +1,15 @@
 ﻿/* Se ejecuta con F5 o con el boton de play
 Live preview es una extension que te permite ver webs con F5 en visual code
 con ctrl +- hace todo mas grande o chico
-git es un software de control de version y github es una plataforma de git que almacena en la nube
+gitt es un software de control de version y github es una plataforma de git que almacena en la nube
 en visual code se crea un repositorio con el icono de control de versiones de la izquierda
 git: rename te permite renombrar una rama
 en git la U al lado de un archivo significa untracked y eso quiere decir que no esta siendo seguido por el repositorio y hay que agregarlo a el
 al hacer un stage sobre estos archivos ahora son seguidos con git y se señala con una A que fueron añadidos al git
+luego se puede hacer un commit que lo que va a hacer es agregar un nuevo  "eslabon" al git con los archivos que fueron modificados desde la ultima version
+Cuando se trabaja en un proyecto, se hace sobre una rama que no sea main, se crea una rama especifica para el conjunto de funciones sobre los que se va a trabajar
+entonces se crea una nueva rama con "create branch"
+las lineas verdes a la izqueirda del codigo reflejan las nuevas lineas, en azul las lineas modificadas y con una flecha roja entre un par de lineas se señala que una linea o mas fueron borradas
+Se puede arriba a la derecha dividir la pantalla en dos o mas, tambien se puede usar la opcion inline view para que se marquen los cambios de codigo aun mas
 Console.WriteLine("Hello, World!");
+
